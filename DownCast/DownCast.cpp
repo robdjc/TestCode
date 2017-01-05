af8d25f699d7113539457c5da1676060b6c25a69
robdjc <robert.dandorf@gmail.com>
2017-01-05 17:36:45 -0500

    adding an override test

--- DownCast/DownCast.cpp
+++ DownCast/DownCast.cpp
@@ -6,22 +6,33 @@ using namespace std;
 class A
 {
 public:
-    virtual void func1() { cout << "A::func1" << endl; };
-    virtual void func2() { cout << "A::func2" << endl; };
+    virtual void func1() { cout << "A::func1\n"; };
+    virtual void func2() { cout << "A::func2\n"; };
+
+    void test_override() { cout << "A::test_override\n"; };
 };
 
 class B : public A
 {
 public:
-    virtual void func1() { cout << "B::func1" << endl; };
-    virtual void func2() { cout << "B::func2" << endl; };
+    virtual void func1() { cout << "B::func1\n"; };
+    virtual void func2() { cout << "B::func2\n"; };
+
+    void test_override() { cout << "B::test_override\n"; };
 };
 
+
+void some_func(A * obj)
+{
+    obj->func1();
+    obj->func2();
+    obj->test_override();
+}
 
 int main()
 {
 
-    cout << "DownCast Test" << endl;
+    cout << "DownCast Test\n";
 
     A* pA = new A;
     pA->func1();
@@ -31,23 +42,30 @@ int main()
     pB->func1();
     pB->func2();
 
+
     // testing undefined behavior
     // gcc/linux this calls A::func1, but there are no guarantees
     B* badB = (B*) pA;
 
-    cout << "Call with a bad B pointer" << endl;
+    cout << "\n\nCall with a bad B pointer\n";
     badB->func1();
 
     B* goodB = dynamic_cast<B*>(pA);
     if(goodB)
     {
-        cout << "dynamic_cast succeeded" << endl;
+        cout << "dynamic_cast succeeded\n";
     }
     else
     {
-        cout << "dynamic_cast failed" << endl;
+        cout << "dynamic_cast failed\n";
     }
 
+    cout << "\n\nTest override\n";
+
+    B myB;
+    A* ptrA = &myB;
+
+    some_func(ptrA);
 
     return 0;
 }
